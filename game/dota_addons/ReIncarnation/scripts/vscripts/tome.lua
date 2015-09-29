9f3fedb9b7e9b312cc195ff1d7c09cc5733f97eb
Dreoh <Tparffrey@gmail.com>
2015-09-29 00:50:42 -0500

    no message

--- game/dota_addons/ReIncarnation/scripts/vscripts/tome.lua
+++ game/dota_addons/ReIncarnation/scripts/vscripts/tome.lua
@@ -5,6 +5,16 @@ function Agi(keys)
 	 Caster:ModifyAgility(3)
 	agility = Caster:GetBaseAgility()
 	print("ProAgility: " .. agility)
+	for i = 0, 5 do
+		if Caster:GetItemInSlot(i) ~= nil and Caster:GetItemInSlot(i):GetName() == "item_custom_tome_agi" then
+			local new_charges = Caster:GetItemInSlot(i):GetCurrentCharges() - 1
+			if new_charges <= 0 then
+    			Caster:RemoveItem(Caster:GetItemInSlot(i))
+			elseif new_charges > 0 then
+				Caster:GetItemInSlot(i):SetCurrentCharges(new_charges)
+			end
+		end
+	end
 end
 
 function Int(keys)
@@ -14,6 +24,16 @@ function Int(keys)
 	Caster:ModifyIntellect(3)
 	intellect = Caster:GetBaseIntellect()
 	print("ProIntellect: " .. intellect)
+	for i = 0, 5 do
+		if Caster:GetItemInSlot(i) ~= nil and Caster:GetItemInSlot(i):GetName() == "item_custom_tome_int" then
+			local new_charges = Caster:GetItemInSlot(i):GetCurrentCharges() - 1
+			if new_charges <= 0 then
+    			Caster:RemoveItem(Caster:GetItemInSlot(i))
+			elseif new_charges > 0 then
+				Caster:GetItemInSlot(i):SetCurrentCharges(new_charges)
+			end
+		end
+	end
 end
 
 function Str(keys)
@@ -23,4 +43,14 @@ function Str(keys)
 	Caster:ModifyStrength(3)
 	strength = Caster:GetBaseStrength()
 	print("ProStrength: " .. strength)
+	for i = 0, 5 do
+		if Caster:GetItemInSlot(i) ~= nil and Caster:GetItemInSlot(i):GetName() == "item_custom_tome_str" then
+			local new_charges = Caster:GetItemInSlot(i):GetCurrentCharges() - 1
+			if new_charges <= 0 then
+    			Caster:RemoveItem(Caster:GetItemInSlot(i))
+			elseif new_charges > 0 then
+				Caster:GetItemInSlot(i):SetCurrentCharges(new_charges)
+			end
+		end
+	end
 end
